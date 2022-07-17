72c348e4527104553aafcd64f87374f162449877
misterbug5 <72231627+misterbug5@users.noreply.github.com>
2022-07-16 23:19:03 -0500

    Experimentation with twind

--- main.ts
+++ main.ts
@@ -7,12 +7,32 @@
 import { InnerRenderFunction, RenderContext, start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 
-import { config, setup } from "@twind";
+import { config, setup, strict } from "@twind";
 import { virtualSheet } from "twind/sheets";
 
 const sheet = virtualSheet();
 sheet.reset();
-setup({ ...config, sheet });
+setup({
+   mode: strict, 
+   theme: {
+    extend:{
+      fontFamily: {
+        sans:['"3270"', 'Helvetica']
+      }
+    }
+    
+   },
+   preflight: {
+    '@font-face': [
+      {
+        fontFamily: '"3270"',
+        fontWeight: '400',
+        src: 'url(/3270%20Semi-Narrow%20Nerd%20Font%20Complete%20Windows%20Compatible.otf)',
+      }
+    ]
+   }, 
+   ...config, sheet
+  });
 
 function render(ctx: RenderContext, render: InnerRenderFunction) {
   const snapshot = ctx.state.get("twind") as unknown[] | null;
